59dfa618d6e5d7867015c39f52f6fed3c087b69c
ayoshida <a.yoshida@xearts.jp>
2020-11-01 02:56:18 +0900

    fix toppage

--- front/src/components/logo.js
+++ front/src/components/logo.js
@@ -1,27 +1,11 @@
 import React from "react";
-import styled from "styled-components";
-
-const Rhombus = styled.div({
-    width: "200px",
-    height: "200px",
-    transform: "RotateZ(45deg)",
-    border: "solid white 5px",
-    margin: "0 auto",
-})
-const Circle = styled.div({
-    width: "120px",
-    height: "120px",
-    borderRadius: "50%",
-    border: "solid 5px white",
-    margin: "0 auto",
-    transform: "translateY(-170px)"
-})
+import "./logo.css";
 
 const Logo = () => {
     return (
         <>
-            <Rhombus />
-            <Circle />
+            <div className="rhombus" />
+            <div className="circle" />
         </>
     );
 }
