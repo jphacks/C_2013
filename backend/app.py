1dbbc8e5a9bf4feb01ff0e6b4f621b1b14a5e1ae
shin04 <daikon0413@gmail.com>
2020-10-31 17:59:43 +0900

    docker-compose

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 
 import io
+import os
 import base64
 
 from middleware.detect import detection
@@ -22,4 +23,5 @@ def upload():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
