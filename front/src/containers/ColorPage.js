7a1882dde6488e2434e29bc7ca33b16c4eed36ab
ayoshida1101 <ayoshida1101@users.noreply.github.com>
2020-11-03 01:10:19 +0000

    Format

--- front/src/containers/ColorPage.js
+++ front/src/containers/ColorPage.js
@@ -1,11 +1,7 @@
 import React from "react";
 
 const ColorPage = () => {
-    return (
-        <div>
-            カラー診断ページです
-        </div>
-    );
+  return <div>カラー診断ページです</div>;
 };
 
 export default ColorPage;
