59c97001cbcce21ea37fbcc78c3dfca8beccbda1
shin04 <daikon0413@gmail.com>
2020-11-03 19:02:13 +0900

    #40 can select model

--- backend/controller/template_controller.py
+++ backend/controller/template_controller.py
@@ -1,10 +1,17 @@
-from ..middleware.fast_detect import detection
+from middleware.fast_detect import detection
+
+
+def surround_eyebrow(img):
+    landmarks, img = detection(img, 'SURROUNDER')
+
+    return landmarks
 
 
 def make_eyebrow(img):
+    surround_eyebrow(img)
     landmarks, img = detection(img)
     for landmark in landmarks:
-        print(landmark)
+        print(len(landmark))
         # ここで眉を切り取って処理
 
     return True, {'message': 'hogehoge'}
